7d5acb2585e6e79c0e7f013ee1b31eca6602eeea
AbedDoesCode <98119874+AbdelrahmanAhmed605@users.noreply.github.com>
2023-01-21 12:05:54 -0800

    Added feature to save and retrieve items in the localStorage to display event information on the screen

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,42 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+/* Obtains all the event information from the localStorage and displays it in their corresponding sections on the 
+page */
+$(window).on('load',function() {
+  var saved_events = JSON.parse(localStorage.getItem("saved_events"));
+  if (saved_events == null) {
+    saved_events = {};
+  }
+
+  for (const event in saved_events) {
+    const eventTimeEl = $(`#${event}`); //Element containing the id representing the event time in the local storage
+    const eventText = saved_events[event];
+    eventTimeEl.children("textarea, .description").text(eventText); //Traverses through the DOM to edit the description of the textarea corresponding to the event time
+  }
+})
+
+var saveBtn = $(".saveBtn");
+
+/* Obtains the time and description of the event when it is saved by the user and saves it to the local storage,
+while also displays the new information on the page */
+saveBtn.on('click', function () {
+  var btnclicked = $(this) //JQuery object of the DOM element that was clicked on (the save button)
+
+  //Traverses through the DOM to obtain the event time (id of the parent element) and 
+  //description(value of the textarea sibling element)
+  var eventTime = btnclicked.parents(".time-block")[0].id;
+  var eventText = btnclicked.siblings("textarea, .description").val();
+
+  var saved_events = JSON.parse(localStorage.getItem("saved_events"));
+  if (saved_events == null) {
+    saved_events = {};
+  }
+  saved_events[eventTime] = eventText;
+  localStorage.setItem("saved_events", JSON.stringify(saved_events));
+})
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
